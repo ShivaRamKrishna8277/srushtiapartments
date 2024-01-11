52683ff1b49fec51270be7b0c324a103f033f571
SHIVARAM KRISHNA <140944124+ShivaRamKrishna8277@users.noreply.github.com>
2024-01-11 18:47:45 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,3 @@ function Menu(value,scroll) {
     document.getElementById("navMobile").style.transform = `translateX(${value})`;
     document.body.style.overflow = `${scroll}`;
 };
-
-const modal = document.getElementById('BookNowForm');
-document.querySelectorAll('.bookroomImgs').forEach((element) => {
-    element.addEventListener('click', () => {
-        modal.show();
-    });
-});
